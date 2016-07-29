d8a4dfd9ed63f6b3abc85ddd642a9d763f551943
ahmar siddiqui <ahmar.siddiqui@gmail.com>
2016-07-29 23:05:47 +0530

    nuked some stuff

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,7 @@ var router = express.Router();
 var wordApi=require('../app/getWords');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
 router.get('/fetch-random-word',function(req,res,next)
 {
 	wordApi.fetchWord().then(function(resp)
